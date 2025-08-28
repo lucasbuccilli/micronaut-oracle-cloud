2c5438db389734f6ff6402b64a7ee4c50d561049
Lucas Buccilli <68135101+lucasbuccilli@users.noreply.github.com>
2025-08-28 12:29:08 -0400

    Update ClientConfigurationCustomizer.java

--- docs-examples/example-java/src/main/java/example/ClientConfigurationCustomizer.java
+++ docs-examples/example-java/src/main/java/example/ClientConfigurationCustomizer.java
@@ -1,3 +1,16 @@
+package example;
+
+import com.oracle.bmc.ClientConfiguration;
+import com.oracle.bmc.retrier.DefaultRetryCondition;
+import com.oracle.bmc.retrier.RetryConfiguration;
+import com.oracle.bmc.waiter.ExponentialBackoffDelayStrategyWithJitter;
+import com.oracle.bmc.waiter.MaxAttemptsTerminationStrategy;
+import io.micronaut.context.event.BeanCreatedEvent;
+import io.micronaut.context.event.BeanCreatedEventListener;
+import jakarta.inject.Singleton;
+
+import java.util.concurrent.TimeUnit;
+
 @Singleton
 public class ConfigurationCustomizer implements BeanCreatedEventListener<ClientConfiguration.ClientConfigurationBuilder> {
     public ClientConfiguration.ClientConfigurationBuilder onCreated(BeanCreatedEvent<ClientConfiguration.ClientConfigurationBuilder> event) {
